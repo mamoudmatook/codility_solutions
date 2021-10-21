7b42ae2334d92359fb74df201f4e5f2f0dd75bf3
Maatuq <maatuq.mhamoud@hcl.com>
2021-10-21 09:11:25 +0400

    (arrays) : solutions to odd occurrencies in array

---
+++ arrays/odd_ocurrencies_in_array.py
@@ -0,0 +1,11 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    result = 0
+    for a in A:
+        result &= a
+    
+    return result
+
+    
